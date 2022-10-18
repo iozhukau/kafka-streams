d5628228adef6d01a093af7b71b949289c99a14c
Ilya Zhukau <iozhukau@gmail.com>
2022-10-18 10:27:23 +0200

    Init

---
+++ src/test/java/net/iozhukau/kafkastreams/KafkaStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.iozhukau.kafkastreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
